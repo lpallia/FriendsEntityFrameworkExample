a09b2c8ad28281d8cd2e28a308731efb0dde8ed8
Luciano Allia <lpallia@gmail.com>
2018-11-06 23:08:17 -0300

    Se muestran los datos agregados en consola

--- EFExample/Program.cs
+++ EFExample/Program.cs
@@ -62,38 +62,78 @@ namespace EFExample
 
             b.Contacts.Add(contact3);
             b.Owner.Add(owner);
+            b.Owner.Add(contact1);
 
-
-            Console.WriteLine("Se va a agegar la agenda");
+            Console.WriteLine("Se va a agegar la agenda 1");
             Console.ReadKey();
 
             IDataAccess<Agenda> dataAccess = new AgendaDataAccess();
             dataAccess.Add(a);
-            dataAccess.Add(b);
 
-            Console.WriteLine("Agenda Agregada");
+            Console.WriteLine("Agenda 1 Agregada");
             Console.ReadKey();
-            Console.WriteLine("Se va a obtener la agenda\n\n");
+            Console.WriteLine("Se va a obtener la agenda 1 \n\n");
             Console.ReadKey();
             Agenda aCopy = dataAccess.Get(a.Id);
 
             Console.WriteLine(aCopy.Id);
             Console.WriteLine(aCopy.Name);
-            //Console.WriteLine(aCopy.Owner.Name);
+
+            Console.WriteLine("Contacts: \n");
 
             foreach (var u in aCopy.Contacts)
             {
                 Console.WriteLine(u.Name);
             }
 
-            Console.ReadKey();
+            Console.WriteLine("\n");
 
-            Console.WriteLine("\n\nSe va a modificar la agenda");
+            Console.WriteLine("Owners de la agenda 1: \n");
+            foreach (var u in aCopy.Owner)
+            {
+                Console.WriteLine(u.Name);
+            }
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Se va a agregar la agenda 2");
+            dataAccess.Add(b);
+
+            Console.WriteLine("Agenda 2 Agregada");
+            Console.ReadKey();
+            Console.WriteLine("Se va a obtener la agenda 2 \n\n");
+            Console.ReadKey();
+            Agenda bCopy = dataAccess.Get(b.Id);
+            
+            Console.WriteLine(bCopy.Id);
+            Console.WriteLine(bCopy.Name);
+
+
+            Console.WriteLine("Contacts: \n");
+
+            foreach (var u in bCopy.Contacts)
+            {
+                Console.WriteLine(u.Name);
+            }
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Owners de la agenda 2: \n");
+            foreach (var u in bCopy.Owner)
+            {
+                Console.WriteLine(u.Name);
+            }
+
+            Console.WriteLine("\n");
+
+            /////
+
+            Console.WriteLine("\n\nSe va a modificar la agenda 1");
             Console.ReadKey();
 
             aCopy.Name = "BLABLA";
-            //aCopy.Owner.Name = "LadySir";
             aCopy.Contacts.Add(new User() { Name = "Kid", Age = 20 });
+            aCopy.Owner.Add(contact3);
 
             dataAccess = new AgendaDataAccess();
             dataAccess.Modify(aCopy);
@@ -106,15 +146,20 @@ namespace EFExample
 
             Console.WriteLine(aCopy2.Id);
             Console.WriteLine(aCopy2.Name);
-            //Console.WriteLine(aCopy2.Owner.Name);
+
+            Console.WriteLine("Contacts: \n");
 
             foreach (var u in aCopy2.Contacts)
             {
                 Console.WriteLine(u.Name);
             }
-
+            
+            Console.WriteLine("Owners de la agenda 1: \n");
+            foreach (var u in aCopy.Owner)
+            {
+                Console.WriteLine(u.Name);
+            }
             Console.ReadKey();
-
 
         }
     }
